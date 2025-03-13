579e0e241235588bc3b2ddc31e57ce09a1a07fbd
juan david <jdacevedo.24@est.ucab.edu.ve>
2025-03-13 13:44:07 -0400

    bucle jugable y mover personaje

--- Mini2D/src/main/Gamepanel.java
+++ Mini2D/src/main/Gamepanel.java
@@ -1,9 +1,9 @@
 package main;
 
-import javax.swing.JPanel;
 import java.awt.*;
+import javax.swing.JPanel;
 
-public class Gamepanel extends JPanel {
+public class Gamepanel extends JPanel implements Runnable{
     // ajustes de pantalla
     final int originalTileSize = 32; //32x32
     final int scale = 2;
@@ -13,10 +13,73 @@ public class Gamepanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    //FPS
+
+    int FPS = 60;
+    KeyHandler KeyH = new KeyHandler();
+    Thread gameThread;
+
+    //Variables del personaje
+    int PlayerX = 100;
+    int PlayerY = 100;
+    int PlayerSpeed = 4;
+
     public Gamepanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(KeyH);
+        this.setFocusable(true);
     }
 
+    public void starGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+    public void update(){
+        if(KeyH.upPressed){
+            PlayerY -= PlayerSpeed;
+        }
+        else if(KeyH.downPressed){
+            PlayerY += PlayerSpeed;
+        }
+        else if(KeyH.leftPressed){
+            PlayerX -= PlayerSpeed;
+        }
+        else if(KeyH.rightPressed){
+            PlayerX += PlayerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.WHITE);
+        g2.fillRect(PlayerX, PlayerY, tileSize, tileSize);
+        g2.dispose();
+    }
 }
--- Mini2D/src/main/Main.java
+++ Mini2D/src/main/Main.java
@@ -14,5 +14,7 @@ public class Main {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.starGameThread();
     }
 }
